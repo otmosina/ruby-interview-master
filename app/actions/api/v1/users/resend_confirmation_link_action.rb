69f47c8e2f4ab775e79db7c515282cd937cb1b9f
otmosina <otmosina@gmail.com>
2021-02-02 11:42:53 +0800

    Use 1 service for send confirmation link

--- app/actions/api/v1/users/resend_confirmation_link_action.rb
+++ app/actions/api/v1/users/resend_confirmation_link_action.rb
@@ -16,7 +16,7 @@ module Api::V1
         def create(input)
           # а вот тут мы описываем возможные ошибки от  каждой монады внутри сервиса
           Try(active_record_common_errors) do
-            ::Users::ResendConfirmationLinkService.new.call(input)
+            ::Users::SendConfirmationLinkService.new.call(input)
             # по идее в сервис можно передать клиент для оправки письма который работает с параметрами to message и так далее
           end.to_result
         end
