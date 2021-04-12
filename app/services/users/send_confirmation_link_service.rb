a68e9db325856838ad8554300fab9bb3fa815ffe
otmosina <otmosina@gmail.com>
2021-04-12 10:26:45 +0800

    t

--- app/services/users/send_confirmation_link_service.rb
+++ app/services/users/send_confirmation_link_service.rb
@@ -11,7 +11,7 @@ module Users
       request = ConfirmationRequest.new(email: email, confirmation_sent_at: DateTime.now)
       email_result = @emailer.with(to: email, confirmation_url: request.confirmation_link).confirmation_email.deliver_later
       request.save!
-
+ 
       nil
     end
   end
